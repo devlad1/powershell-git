f89989873f370f4c663fba218b24a7600d6f4d36
vlad <44703928+devlad1@users.noreply.github.com>
2024-01-27 21:49:21 +0200

    Update example.ps1

--- examples/example.ps1
+++ examples/example.ps1
@@ -1,11 +1,11 @@
-# This is is an example usage.
+ # This is is an example usage.
 # Put this code in a powershell profile
 # The usual path is '$Home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'
 
 # Load or copy and paste the '__git_ps1' function here
 
 function prompt {
-    Write-Host -NoNewline -ForegroundColor Cyan "$(Get-Location) "
+	Write-Host -NoNewline -ForegroundColor Cyan "$(Get-Location) "
 	Write-Host -NoNewline -ForegroundColor Green "($(__git_ps1)) "
 	
 	return "$ "
